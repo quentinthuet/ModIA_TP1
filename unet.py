00cf3e79965ab0be3b6cc9a00ccd7260adecf9a5
Quentin Thuet <quentin.thuet@gmail.com>
2022-06-03 14:17:28 +0200

    added get_features unet

--- unet.py
+++ unet.py
@@ -72,6 +72,11 @@ class UNet(nn.Module):
         
         return out
 
+    def get_features(self, x):
+        x, _ = self.downsample_block_1(x)
+        x, _ = self.downsample_block_2(x)
+        x, _ = self.downsample_block_3(x)
+        return x
         
 if __name__=='__main__':
     x = torch.rand(16,1,224,224)
